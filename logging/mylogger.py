50c13b40c48be423b8193282cca26a4040c770d9
matthewferler <matthew.f.erler@gmail.com>
2023-07-08 17:53:42 -0500

    Refactored code to simplify it

--- logging/mylogger.py
+++ logging/mylogger.py
@@ -2,14 +2,6 @@ import io
 import logging
 import traceback
 import types
-from datetime import datetime
-
-
-class TimestampFormatter(logging.Formatter):
-    def format(self, record):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f"[{now}] {record.levelname}: {record.msg}"
-        return message
 
 
 def stack_trace(self, ex):
@@ -22,10 +14,11 @@ def get_logger(log_level):
     if 'my_logger' in logging.Logger.manager.loggerDict:
         return logging.getLogger('my_logger')
     else:
+        logging.basicConfig(filename='application.log',
+                            level=log_level,
+                            format='%(asctime)s %(levelname)s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
         logger = logging.getLogger('my_logger')
         logger.setLevel(log_level)
-        handler = logging.FileHandler('application.log')
-        handler.setFormatter(TimestampFormatter())
-        logger.addHandler(handler)
         logger.stack_trace = types.MethodType(stack_trace, logger)
         return logger
