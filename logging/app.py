04d30e4e42bf92ef11eb31f7e89dd7e8fd386c8e
matthewferler <matthew.f.erler@gmail.com>
2023-07-08 17:03:47 -0500

    pep8 formatting

--- logging/app.py
+++ logging/app.py
@@ -4,10 +4,10 @@ import mylogger
 logger = mylogger.get_logger(log_level=logging.DEBUG)
 
 try:
-  logger.info("Application entered")
-  x =  1 / 0
+    logger.info("Application entered")
+    x = 1 / 0
 except Exception as e:
-  logger.stack_trace(e)
+    logger.stack_trace(e)
 
 
 logger.info("Application exiting")
--- logging/mylogger.py
+++ logging/mylogger.py
@@ -1,27 +1,31 @@
 import io
 import logging
-from datetime import datetime
 import traceback
 import types
+from datetime import datetime
+
 
 class TimestampFormatter(logging.Formatter):
-  def format(self, record):
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    message = f"[{now}] {record.levelname}: {record.msg}"
-    return message
+    def format(self, record):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f"[{now}] {record.levelname}: {record.msg}"
+        return message
+
+
 def stack_trace(self, ex):
-  buffer = io.StringIO()
-  traceback.print_exception(type(ex), ex, ex.__traceback__, file=buffer)
-  self.error(buffer.getvalue())
+    buffer = io.StringIO()
+    traceback.print_exception(type(ex), ex, ex.__traceback__, file=buffer)
+    self.error(buffer.getvalue())
+
 
 def get_logger(log_level):
-  if 'my_logger' in logging.Logger.manager.loggerDict:
-    return logging.getLogger('my_logger')
-  else:
-    logger = logging.getLogger('my_logger')
-    logger.setLevel(log_level)
-    handler = logging.FileHandler('application.log')
-    handler.setFormatter(TimestampFormatter())
-    logger.addHandler(handler)
-    logger.stack_trace = types.MethodType(stack_trace, logger)
-    return logger
+    if 'my_logger' in logging.Logger.manager.loggerDict:
+        return logging.getLogger('my_logger')
+    else:
+        logger = logging.getLogger('my_logger')
+        logger.setLevel(log_level)
+        handler = logging.FileHandler('application.log')
+        handler.setFormatter(TimestampFormatter())
+        logger.addHandler(handler)
+        logger.stack_trace = types.MethodType(stack_trace, logger)
+        return logger
